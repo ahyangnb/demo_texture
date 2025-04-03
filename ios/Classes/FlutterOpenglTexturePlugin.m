ac9d7d9ed902a1c1aa7bd111139e7c1aa1e16086
q1 <zonggeu@gmail.com>
2025-04-03 14:57:57 +0800

    languege set

---
+++ ios/Classes/FlutterOpenglTexturePlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterOpenglTexturePlugin.h"
+
+@implementation FlutterOpenglTexturePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_opengl_texture"
+            binaryMessenger:[registrar messenger]];
+  FlutterOpenglTexturePlugin* instance = [[FlutterOpenglTexturePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
